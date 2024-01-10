bdf39a7de7db02adc1c7dba58464e38bd29e7ba8
jevin1212 <jvnjvn222@gmail.com>
2024-01-10 11:18:37 +0530

    map screen done

--- lib/Utility/assets_utility.dart
+++ lib/Utility/assets_utility.dart
@@ -5,17 +5,28 @@ import 'package:local_first/generated/assets.dart';
 
 ExactAssetImage imagesBg = const ExactAssetImage(Assets.imagesBg, scale: 4);
 ExactAssetImage imagesBg1 = const ExactAssetImage(Assets.imagesBg1, scale: 4);
-ExactAssetImage imagesAppLogo = const ExactAssetImage(Assets.imagesAppLogo, scale: 4);
-ExactAssetImage imagesOnBoarding1 = const ExactAssetImage(Assets.imagesOnBoarding1, scale: 4);
-ExactAssetImage imagesOnBoarding2 = const ExactAssetImage(Assets.imagesOnBoarding2, scale: 4);
-ExactAssetImage imagesOnBoarding3 = const ExactAssetImage(Assets.imagesOnBoarding3, scale: 4);
-ExactAssetImage imagesPasswordPic = const ExactAssetImage(Assets.imagesPasswordPic, scale: 4);
-ExactAssetImage imagesMailPic = const ExactAssetImage(Assets.imagesMailPic, scale: 4);
-ExactAssetImage imagesAccessLocation = const ExactAssetImage(Assets.imagesAccessLocation, scale: 4);
-
-
+ExactAssetImage imagesAppLogo =
+    const ExactAssetImage(Assets.imagesAppLogo, scale: 4);
+ExactAssetImage imagesOnBoarding1 =
+    const ExactAssetImage(Assets.imagesOnBoarding1, scale: 4);
+ExactAssetImage imagesOnBoarding2 =
+    const ExactAssetImage(Assets.imagesOnBoarding2, scale: 4);
+ExactAssetImage imagesOnBoarding3 =
+    const ExactAssetImage(Assets.imagesOnBoarding3, scale: 4);
+ExactAssetImage imagesPasswordPic =
+    const ExactAssetImage(Assets.imagesPasswordPic, scale: 4);
+ExactAssetImage imagesMailPic =
+    const ExactAssetImage(Assets.imagesMailPic, scale: 4);
+ExactAssetImage imagesAccessLocation =
+    const ExactAssetImage(Assets.imagesAccessLocation, scale: 4);
 
 /// Icons
 
-ExactAssetImage iconsFacebook = const ExactAssetImage(Assets.iconsFacebook, scale: 4);
-ExactAssetImage iconsGoogle = const ExactAssetImage(Assets.iconsGoogle, scale: 4);
+ExactAssetImage iconsFacebook =
+    const ExactAssetImage(Assets.iconsFacebook, scale: 4);
+ExactAssetImage iconsGoogle =
+    const ExactAssetImage(Assets.iconsGoogle, scale: 4);
+ExactAssetImage searchBackIcon =
+    const ExactAssetImage(Assets.iconsSearchBackIcon, scale: 4);
+ExactAssetImage markerIcon =
+    const ExactAssetImage(Assets.iconsMarkerIcon, scale: 4);
---
+++ lib/View/Authentication/select_location_screen.dart
@@ -0,0 +1,104 @@
+import 'package:local_first/View/Authentication/map_screen.dart';
+
+import '../../Utility/utility_export.dart';
+import '../../generated/assets.dart';
+
+class SelectLocationScreen extends StatefulWidget {
+  const SelectLocationScreen({super.key});
+
+  @override
+  State<SelectLocationScreen> createState() => _SelectLocationScreenState();
+}
+
+class _SelectLocationScreenState extends State<SelectLocationScreen> {
+  TextEditingController textEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return commonStructure(
+        context: context,
+        bgColor: white,
+        appBar: AppBar(
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 18.0),
+            child: Image.asset(
+              Assets.iconsBackIcon,
+              scale: 4,
+            ),
+          ),
+          title: Text(
+            'Select Location',
+            style: AppFontStyle.blackOpenSans18W600,
+          ),
+          centerTitle: true,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            children: [
+              commonTextField(
+                  hintText: 'Search location',
+                  hintStyle: AppFontStyle.greyOpenSans14W400,
+                  textEditingController: textEditingController,
+                  preFixIcon:
+                      const ExactAssetImage(Assets.iconsSearchIcon, scale: 4),
+                  suffixIcon: Image.asset(
+                    Assets.iconsGpsIcon,
+                    scale: 4,
+                  )),
+              customHeight(18),
+              Expanded(
+                child: ListView.separated(
+                    itemCount: 2,
+                    shrinkWrap: true,
+                    separatorBuilder: (context, index) {
+                      return const Divider(
+                        thickness: 0.5,
+                      );
+                    },
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        onTap: () {
+                          Get.to(() => const MapScreen());
+                        },
+                        contentPadding: EdgeInsets.zero,
+                        leading: Container(
+                          width: 40,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                                width: 1, color: black.withOpacity(0.1)),
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(100.0),
+                            ),
+                          ),
+                          child: Image.asset(
+                            Assets.iconsLocationIcon,
+                            scale: 4,
+                          ),
+                        ),
+                        title: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Allentown',
+                              style: AppFontStyle.blackOpenSans12W500,
+                            ),
+                            Text(
+                              '5.5 KM Away',
+                              style: AppFontStyle.blackOpenSans12W500,
+                            )
+                          ],
+                        ),
+                        subtitle: Text(
+                          '2118 Thornridge Cir. Syracuse, Connecticut 35624',
+                          style: AppFontStyle.greyOpenSans12W500,
+                        ),
+                      );
+                    }),
+              )
+            ],
+          ),
+        ));
+  }
+}
